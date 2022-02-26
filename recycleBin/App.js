6083aa3f5f21da87bc6d585191434a90005003cd
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-02-26 21:46:59 +0600

    show image

--- recycleBin/App.js
+++ recycleBin/App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
-  const handlePress=()=>{
-    console.log("hello")
-  }
+  console.log(require("./assets/icon.png"));
+  const handlePress = () => {
+    console.log("hello");
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Open up App.js to start working on your app!</Text>
+      <Text onPress={handlePress}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
+      <Image source={require("./assets/icon.png")} />
     </SafeAreaView>
   );
 }
@@ -16,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
