d9238f096cc5ff61f5dd99faab8782ebf5b428cb
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-02-28 22:09:41 +0600

    statusBar

--- recycleBin/App.js
+++ recycleBin/App.js
@@ -1,93 +1,40 @@
-import React, { useState } from "react";
-import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Alert,
+  Button, Platform, StatusBar
+} from "react-native";
 
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  
-   {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53apb28ba",
-    title: "fourth Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97p63",
-    title: "fifth Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29p72",
-    title: "sixth Item",
-  },
-];
-
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-    <Text style={[styles.title, textColor]}>{item.title}</Text>
-  </TouchableOpacity>
-);
-
-const App = () => {
-  const [selectedId, setSelectedId] = useState(null);
-
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
-    const color = item.id === selectedId ? 'white' : 'black';
-
-    return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={{ backgroundColor }}
-        textColor={{ color }}
-      />
-    );
+export default function App() {
+  const clickYes = () => {
+    console.log("yes clicked");
   };
+  console.log(StatusBar.currentHeight);
+  const handlePress = () => {
+    //to show an alert
+    Alert.alert("my title", "button clicked", [
+      { text: "yes", onPress: clickYes},
+      { text: "no" , onPress: ()=>console.log("no clicked")},
+    ]);
 
+   
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-      horizontal={true}
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
-        showsHorizontalScrollIndicator={false} //to hide the scrollbar
-      />
-      
-       <FlatList
-      horizontal={true}
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
-      />
+      <StatusBar style="auto" />
+      <Button title="click" onPress={handlePress} />
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    height: 93
-  },
-  title: {
-    fontSize: 32,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  
   },
 });
-
-export default App;
