41c62cb9c0bccf44f8b587a5413e39caa34421aa
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-03-05 14:55:46 +0600

    started working on the project

--- recycleBin/App.js
+++ recycleBin/App.js
@@ -1,24 +1,10 @@
 import { StyleSheet, Text, View, SafeAreaView, Alert } from "react-native";
+import { WelcomeScreen } from "./app/screen/WelcomeScreen";
 
 export default function App() {
-  const clickYes = () => {
-    console.log("yes clicked");
-  };
-  console.log(" ");
-  const handlePress = () => {
-    //to show an alert
-    Alert.alert("my title", "button clicked", [
-      { text: "yes", onPress: clickYes },
-      { text: "no", onPress: () => console.log("no clicked") },
-    ]);
-  };
+   
   return (
-    <View style={styles.container}>
-      {/* in potrait mode, flex is vertical by default */}
-      <View style={{ backgroundColor: "#12342a", width:100,height:100 }} />
-      <View style={{ backgroundColor: "gold", width:100,height:100 }} />
-      <View style={{ backgroundColor: "tomato", width:100,height:100 }} />
-    </View>
+    <WelcomeScreen/>
   );
 }
 
@@ -26,8 +12,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-    flexDirection:"row-reverse"
+    alignItems: "center",
+    justifyContent: "center", //align item with main axis
+    // flexDirection:"row-reverse"
   },
 });
