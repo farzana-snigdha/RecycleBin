f7321712de6b711c45e3c6999ebb8daafe1ecd80
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-03-06 13:42:18 +0600

    upto 46

--- recycleBin/app/screen/WelcomeScreen.js
+++ recycleBin/app/screen/WelcomeScreen.js
@@ -11,6 +11,7 @@ export const WelcomeScreen = (props) => {
   const { container } = styles;
   return (
     <ImageBackground
+    blurRadius={10}
       style={styles.background}
       source={require("../assets/backgroung.jpg")}
     >
@@ -19,36 +20,39 @@ export const WelcomeScreen = (props) => {
           style={styles.logo}
           source={require("../assets/sale-logo.png")}
         />
-        <Text>Sell what you don't need</Text>
+        <Text style={styles.tagline}>Sell what you don't need</Text>
       </View>
       <View style={styles.buttonContainer}>
-
-      <AppButton title="Login"/>
-      <AppButton title="Register" color="secondary"/>
-      
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
       </View>
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  logoContainer: {
-    position: "absolute",
-    top: 60,
-    alignItems: "center",
-  },
   background: {
     flex: 1,
     // to put the btn at the bottom of the screen
     alignItems: "center",
     justifyContent: "flex-end",
   },
-  buttonContainer:{
-    padding:20, width:"100%",
+  buttonContainer: {
+    padding: 20,
+    width: "100%",
   },
-  regBtn: { width: "100%", height: 70, backgroundColor: colors.secondary },
+  logoContainer: {
+    position: "absolute",
+    top: 60,
+    alignItems: "center",
+  },
   logo: {
     width: 100,
     height: 100,
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
 });
