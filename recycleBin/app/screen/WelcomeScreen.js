3f8e6e8a44397417e60905b7d1fb1d233a955d13
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-03-06 11:29:05 +0600

    Update WelcomeScreen.js

--- recycleBin/app/screen/WelcomeScreen.js
+++ recycleBin/app/screen/WelcomeScreen.js
@@ -1,5 +1,8 @@
-import React from "react";
-import { View, Text, StyleSheet, ImageBackground, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from 'react'
+import { KeyboardAvoidingView,  TextInput, TouchableOpacity,View, Text, StyleSheet, ImageBackground, Image  } from 'react-native'
+import { auth } from '../../firebase'
+
 
 /**
  * @author
@@ -7,28 +10,92 @@ import { View, Text, StyleSheet, ImageBackground, Image } from "react-native";
  **/
 export const WelcomeScreen = (props) => {
   const { container } = styles;
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigation.navigate("Home")
+      }
+    })
+
+    return unsubscribe
+  }, [])
+
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log('Registered with:', user.email);
+      })
+      .catch(error => alert(error.message))
+  }
+
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log('Logged in with:', user.email);
+      })
+      .catch(error => alert(error.message))
+  }
   return (
     <ImageBackground
       style={styles.background}
       source={require("../assets/backgroung.jpg")}
     >
-        <View style={styles.logoContainer}>
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/sale-logo.png")}
+        />
+        <Text>Sell what you don't need</Text>
+      </View>
 
-      <Image style={styles.logo} source={require("../assets/sale-logo.png")} />
-      <Text>Sell what you don't need</Text>
-        </View>
-      <View style={styles.loginBtn}></View>
-      <View style={styles.regBtn}></View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+     <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-    logoContainer: {
-        position: "absolute",
-        top: 60,
-   
-    
+  logoContainer: {
+    position: "absolute",
+    top: 60,
+
     alignItems: "center",
   },
   background: {
@@ -46,7 +113,45 @@ const styles = StyleSheet.create({
   logo: {
     width: 100,
     height: 100,
-   
+  },
 
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
   },
 });
