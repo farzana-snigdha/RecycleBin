2db3f0c4bbe0573f2ae8d0890988c71beef32cf8
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-03-06 12:44:48 +0600

    35. ViewImageScreen

--- recycleBin/App.js
+++ recycleBin/App.js
@@ -5,18 +5,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './app/screen/LoginScreen';
 import HomeScreen from './app/screen/HomeScreen';
 import { WelcomeScreen } from './app/screen/WelcomeScreen';
+import ViewImageScreen from './app/screen/ViewImageScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-      {/* <Stack.Screen options={{ headerShown: false }} name="Welcome" component={WelcomeScreen} /> */}
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ViewImageScreen/>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //   {/* <Stack.Screen options={{ headerShown: false }} name="Welcome" component={WelcomeScreen} /> */}
+    //     <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+    //     <Stack.Screen name="Home" component={HomeScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 }
 
