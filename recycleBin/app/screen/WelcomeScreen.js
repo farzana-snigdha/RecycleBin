c008c10a3de248266cb3ad39bccc909ccfebaa92
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-03-06 11:38:22 +0600

    auth is moved to another component

--- recycleBin/app/screen/WelcomeScreen.js
+++ recycleBin/app/screen/WelcomeScreen.js
@@ -2,6 +2,7 @@ import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from 'react'
 import { KeyboardAvoidingView,  TextInput, TouchableOpacity,View, Text, StyleSheet, ImageBackground, Image  } from 'react-native'
 import { auth } from '../../firebase'
+import { AuthComponent } from "../components/AuthComponent";
 
 
 /**
@@ -57,36 +58,7 @@ export const WelcomeScreen = (props) => {
         <Text>Sell what you don't need</Text>
       </View>
 
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={text => setEmail(text)}
-          style={styles.input}
-        />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={text => setPassword(text)}
-          style={styles.input}
-          secureTextEntry
-        />
-      </View>
-
-     <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={handleLogin}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={handleSignUp}
-          style={[styles.button, styles.buttonOutline]}
-        >
-          <Text style={styles.buttonOutlineText}>Register</Text>
-        </TouchableOpacity>
-      </View>
+      <AuthComponent/>
     </ImageBackground>
   );
 };
@@ -115,43 +87,5 @@ const styles = StyleSheet.create({
     height: 100,
   },
 
-  inputContainer: {
-    width: '80%'
-  },
-  input: {
-    backgroundColor: 'white',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 5,
-  },
-  buttonContainer: {
-    width: '60%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40,
-  },
-  button: {
-    backgroundColor: '#0782F9',
-    width: '100%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  buttonOutline: {
-    backgroundColor: 'white',
-    marginTop: 5,
-    borderColor: '#0782F9',
-    borderWidth: 2,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-  buttonOutlineText: {
-    color: '#0782F9',
-    fontWeight: '700',
-    fontSize: 16,
-  },
+ 
 });
