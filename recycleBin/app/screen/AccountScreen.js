013470a75779d5330f9c0488b666e36ca0162ea6
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-03-18 12:39:12 +0600

    added user account screen

---
+++ recycleBin/app/screen/AccountScreen.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { StyleSheet, View, FlatList, StatusBar } from "react-native";
+
+import ListItem from "../components/ListItem";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+import ListItemSeparator from "../components/ListItemSeparator";
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
+
+function AccountScreen(props) {
+  return (<View style={styles.screen}>
+    <StatusBar/>
+      <View style={styles.container}>
+        <ListItem
+          title="Mosh Hamedani"
+          subTitle="programmingwithmosh@gmail.com"
+          image={require("../assets/sloth.jpg")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          ItemSeparatorComponent={ListItemSeparator}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+        />
+      </View>
+      <ListItem
+        title="Log Out"
+        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+      />
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex:1,
+    backgroundColor: colors.lightGrey,
+  },
+  container: {
+    marginVertical: 20,
+  },
+});
+
+export default AccountScreen;
