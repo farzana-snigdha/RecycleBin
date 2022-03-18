6a0dec65e9f00287f861ef78688465996188e949
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-03-18 13:02:10 +0600

    Update ItemListScreen.js

--- recycleBin/app/screen/ItemListScreen.js
+++ recycleBin/app/screen/ItemListScreen.js
@@ -1,17 +1,47 @@
-import { View, Text, StatusBar } from "react-native";
+import { View, Text, StatusBar, StyleSheet, FlatList } from "react-native";
 import React from "react";
 import { Card } from "react-native-elements";
+
 import { CardComponent } from "../components/CardComponent";
+import colors from "../config/colors";
+const listings = [
+  {
+    id: 1,
+    title: "Red jacket for sale",
+    price: 100,
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Couch in great condition",
+    price: 1000,
+    image: require("../assets/couch.jpg"),
+  },
+];
 
 export default function ItemListScreen() {
+
   return (
-    <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 60 }}>
+    <View style={styles.screen}>
       <StatusBar />
-      <CardComponent
-        title="jacket"
-        subTitle="$100"
-        image={require("../assets/jacket.jpg")}
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => 
+          <CardComponent
+          title={item.title}
+          subTitle={"$" + item.price}
+          image={item.image}
+          /> 
+        }
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    backgroundColor: colors.lightGrey,
+  },
+});
