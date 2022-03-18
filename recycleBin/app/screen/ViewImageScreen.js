08dca5fa28e3f2463e5b592ae7fd577568db1e14
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-03-18 10:53:26 +0600

    Update ViewImageScreen.js

--- recycleBin/app/screen/ViewImageScreen.js
+++ recycleBin/app/screen/ViewImageScreen.js
@@ -1,13 +1,22 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../config/colors";
 
 const ViewImageScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon}></View>
-      <View style={styles.deleteIcon}></View>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" color={colors.primary} size={40} />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          color={colors.secondary}
+          size={40}
+        />
+      </View>
       <Image
         resizeMode="contain"
         style={styles.image}
@@ -19,18 +28,12 @@ const ViewImageScreen = () => {
 
 const styles = StyleSheet.create({
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
   },
   container: { backgroundColor: colors.black, flex: 1 },
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
