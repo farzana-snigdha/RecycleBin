102b4582a038e5fa0b4f064678efe07647923c2a
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-03-18 09:45:26 +0600

    Update AuthNavigation.js

--- recycleBin/app/navigation/AuthNavigation.js
+++ recycleBin/app/navigation/AuthNavigation.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screen/LoginScreen";
 import HomeScreen from "../screen/HomeScreen";
 import RegisterScreen from "../screen/RegisterScreen";
+import { WelcomeScreen } from "../screen/WelcomeScreen";
 
 const AuthNavigation = () => {
   const Stack = createNativeStackNavigator();
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen options={{ headerShown: false }} name="Welcome" component={WelcomeScreen} />
       <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
