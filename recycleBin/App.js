50d35f132e86e3430e98ca4331df3b8531178a56
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-09 21:43:53 +0600

    Update App.js

--- recycleBin/App.js
+++ recycleBin/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -11,16 +11,22 @@ import ItemListScreen from "./app/screen/ItemListScreen";
 import ListingDetailsScreen from "./app/screen/ListingDetailsScreen";
 import MessagesScreen from "./app/screen/MessagesScreen";
 import AccountScreen from "./app/screen/AccountScreen";
+import { TextInput } from "react-native-gesture-handler";
+import AppTextInput from "./app/components/AppTextInput";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [first, setFirst] = useState("");
   return (
     // <NavRoute/>
     // <ListingDetailsScreen />
 
     // <ViewImageScreen/>
-    <ItemListScreen/> // aka listing screens in vid 64
- 
+    // <ItemListScreen/> // aka listing screens in vid 64
+    <View>
+      <StatusBar />
+      <AppTextInput placeholder="Username" icon="email" />
+    </View> 
   );
 }
 
