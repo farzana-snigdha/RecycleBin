a26b28ff779e8c0c7c71c71cd2fd6c9503a670bb
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-10 00:47:11 +0600

    Update AppPicker.js

--- recycleBin/app/components/AppPicker.js
+++ recycleBin/app/components/AppPicker.js
@@ -29,9 +29,11 @@ function AppPicker({ icon, items, onSelectItem, placeholder, selectedItem }) {
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>
-            {selectedItem ? selectedItem.label : placeholder}
-          </AppText>
+          {selectedItem ? (
+            <AppText style={styles.text}>{selectedItem.label}</AppText>
+          ) : (
+            <AppText style={styles.placeholder}>{placeholder}</AppText>
+          )}
           <MaterialCommunityIcons
             name="chevron-down"
             size={25}
@@ -71,6 +73,10 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10,
+  },
+  placeholder: {
+    color: defaultStyles.colors.mediumGrey,
+    flex: 1,
   },
   text: {
     flex: 1,
