c0f62667dd7e59c0475b4a22d9999dacdf7756a9
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-09 21:48:37 +0600

    Update styles.js

--- recycleBin/app/config/styles.js
+++ recycleBin/app/config/styles.js
@@ -2,6 +2,7 @@ import { Platform } from "react-native";
 import colors from "./colors";
 
 export default {
+    colors,
   text: {
     fontSize: 18,
     fontFamily:Platform.OS === "android" ? "Roboto" : "Avenir",
