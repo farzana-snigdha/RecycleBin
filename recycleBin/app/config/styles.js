cdbb32af57c4a978f48e1f02aec1fc7c332013e6
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-09 21:43:18 +0600

    added text style

--- recycleBin/app/config/styles.js
+++ recycleBin/app/config/styles.js
@@ -2,5 +2,9 @@ import { Platform } from "react-native";
 import colors from "./colors";
 
 export default {
-
+  text: {
+    fontSize: 18,
+    fontFamily:Platform.OS === "android" ? "Roboto" : "Avenir",
+    color: colors.darkGrey,
+  },
 };
