f6e075d1f009138babaf4ac7302b62bcb5ea41b6
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-09 23:08:07 +0600

    Update styles.js

--- recycleBin/app/config/styles.js
+++ recycleBin/app/config/styles.js
@@ -2,10 +2,10 @@ import { Platform } from "react-native";
 import colors from "./colors";
 
 export default {
-    colors,
+  colors,
   text: {
     fontSize: 18,
-    fontFamily:Platform.OS === "android" ? "Roboto" : "Avenir",
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
     color: colors.darkGrey,
   },
 };
