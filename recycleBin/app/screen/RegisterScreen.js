5cf420c8d5fd83e2c6bcf88fc8c8c3291ec250fb
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-10 00:16:38 +0600

    Create RegisterScreen.js

---
+++ recycleBin/app/screen/RegisterScreen.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+import * as Yup from "yup";
+
+import {
+  AppForm as Form,
+  AppFormField as FormField,
+  SubmitButton,
+} from "../components/forms";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(6).label("Password"),
+  passwordConfirmation: Yup.string()
+     .oneOf([Yup.ref('password'), null], 'Passwords must match').label("PasswordConfirmation"),
+});
+
+function RegisterScreen() {
+  return (
+    <View style={styles.container}>
+     <StatusBar/>
+      <Form
+        initialValues={{ name: "", email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <FormField
+          autoCorrect={false}
+          icon="account"
+          name="name"
+          placeholder="Name"
+        />
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          secureTextEntry
+          textContentType="password"
+        />
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="passwordConfirmation"
+          placeholder="Retype Pass"
+          secureTextEntry
+          textContentType="password"
+        />
+        <SubmitButton title="Register" />
+      </Form>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
+
+export default RegisterScreen;
