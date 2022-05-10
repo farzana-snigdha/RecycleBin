6b6c9702c0a44db6664e7a36ff3c53bb575b4721
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-10 15:04:31 +0600

    Update App.js

--- recycleBin/App.js
+++ recycleBin/App.js
@@ -1,30 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { StyleSheet, Text, View, StatusBar, Button,Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-
 import * as ImagePicker from "expo-image-picker";
-import * as Permissions from 'expo-permissions'
+import * as Permissions from "expo-permissions";
+import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   // const [category, setCategory] = useState(categories[0]);
-
-  const requestPermission = async () => {
-    // const result =await Permissions.askAsync(Permissions.MEDIA_LIBRARY, Permissions.LOCATION_BACKGROUND)
-    const {granted} = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted) {
-      alert("You need to enable permission to access the library");
-    }
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  return <View></View>;
+  const [imageUris, setImageUris] = useState([]);
+ 
+const handleAdd=uri=>{
+  setImageUris([...imageUris,uri])
+}
+const handleRemove=uri=>{
+  setImageUris(imageUris.filter(imageUri=>imageUri!==uri))
+}
+ 
+  return (
+    <View style={styles.container}>
+      <StatusBar />
+      {/* <ImageInput imageUri={imageUris} onChangeImage={uri=>setImageUris(uri)} /> */}
+      <ImageInputList
+      imageUris={imageUris}
+      onAddImage={handleAdd}
+      onRemoveImage={handleRemove}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
