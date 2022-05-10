181ae51472ce5feff3be566e573a842ed28a309a
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-10 12:30:48 +0600

    custom code snippet, type rnft

---
+++ recycleBin/app/components/Test.js
@@ -0,0 +1,16 @@
+import { StyleSheet,  View } from 'react-native'
+import React from 'react'
+
+export default function Test() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+
+    },
+})
