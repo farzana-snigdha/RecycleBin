ee7bcf4e22213641991de0aba51c93ab96dc1f1d
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-10 19:40:29 +0600

    Update AccountNavigator.js

--- recycleBin/app/navigation/AccountNavigator.js
+++ recycleBin/app/navigation/AccountNavigator.js
@@ -6,8 +6,8 @@ import MessagesScreen from "../screen/MessagesScreen";
 const Stack = createStackNavigator();
 
 const AccountNavigator = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="Account Details" component={AccountScreen} />
+  <Stack.Navigator>
+    <Stack.Screen name="Account" component={AccountScreen} />
     <Stack.Screen name="Messages" component={MessagesScreen} />
   </Stack.Navigator>
 );
