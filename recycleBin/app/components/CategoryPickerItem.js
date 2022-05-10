f3093eeecd3d8f5f1ca224cbe45988a0aee9c7da
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-10 12:01:17 +0600

    Update CategoryPickerItem.js

--- recycleBin/app/components/CategoryPickerItem.js
+++ recycleBin/app/components/CategoryPickerItem.js
@@ -1,26 +1,24 @@
-import { StyleSheet,  View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 import Icon from "./Icon";
 import { AppText } from "./AppTextComponent";
 
 export default function CategoryPickerItem({ item, onPress }) {
-  console.log(item.label)
+  console.log(item.label);
   return (
     <View style={styles.conatiner}>
-      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80}/>
-        <AppText style={styles.label}>{item.label}</AppText>
-      
+      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
+      <AppText style={styles.label}>{item.label}</AppText>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   conatiner: {
-    paddingHorizontal:15,
-    paddingVertical:15,
-    alignItems:"center",
-    width:"33%",
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    alignItems: "center",
+    width: "33%",
   },
-  label:{marginTop:5,
-  textAlign:"center",}
+  label: { marginTop: 5, textAlign: "center" },
 });
