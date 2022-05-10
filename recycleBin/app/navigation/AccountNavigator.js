30d034f86aa81549a6a9b693b23348fc05c875e1
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-10 19:13:44 +0600

    Create AccountNavigator.js

---
+++ recycleBin/app/navigation/AccountNavigator.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import AccountScreen from "../screen/AccountScreen";
+import MessagesScreen from "../screen/MessagesScreen";
+
+const Stack = createStackNavigator();
+
+const AccountNavigator = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Account Details" component={AccountScreen} />
+    <Stack.Screen name="Messages" component={MessagesScreen} />
+  </Stack.Navigator>
+);
+
+export default AccountNavigator;
