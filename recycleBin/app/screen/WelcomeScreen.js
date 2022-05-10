fac1a694aed07adab590050af37b2ba1eb1e9aa3
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-10 18:42:12 +0600

    Update WelcomeScreen.js

--- recycleBin/app/screen/WelcomeScreen.js
+++ recycleBin/app/screen/WelcomeScreen.js
@@ -1,54 +1,53 @@
 import React from "react";
-import { View, Text, StyleSheet, ImageBackground, Image } from "react-native";
-import AppButton from "../components/AppButton";
-import colors from "../config/colors";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 
-/**
- * @author
- * @function WelcomeScreen
- **/
-export const WelcomeScreen = (props) => {
-  const { container } = styles;
+import Button from "../components/Button";
+import routes from "../navigation/routes";
+
+function WelcomeScreen({ navigation }) {
   return (
     <ImageBackground
-    blurRadius={10}
+      blurRadius={10}
       style={styles.background}
-      source={require("../assets/backgroung.jpg")}
+      source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
-        <Image
-          style={styles.logo}
-          source={require("../assets/sale-logo.png")}
-        />
-        <Text style={styles.tagline}>Sell what you don't need</Text>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.buttonContainer}>
-        <AppButton title="Login" />
-        <AppButton title="Register" color="secondary" />
+      <View style={styles.buttonsContainer}>
+        <Button
+          title="Login"
+          onPress={() => navigation.navigate(routes.LOGIN)}
+        />
+        <Button
+          title="Register"
+          color="secondary"
+          onPress={() => navigation.navigate(routes.REGISTER)}
+        />
       </View>
     </ImageBackground>
   );
-};
+}
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    // to put the btn at the bottom of the screen
-    alignItems: "center",
     justifyContent: "flex-end",
+    alignItems: "center",
   },
-  buttonContainer: {
+  buttonsContainer: {
     padding: 20,
     width: "100%",
-  },
-  logoContainer: {
-    position: "absolute",
-    top: 60,
-    alignItems: "center",
   },
   logo: {
     width: 100,
     height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
   },
   tagline: {
     fontSize: 25,
@@ -56,3 +55,5 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
   },
 });
+
+export default WelcomeScreen;
