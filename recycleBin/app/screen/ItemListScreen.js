833f0b6b73b95a33967f6dce6274737c57f6332a
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-13 11:14:24 +0600

    Update ItemListScreen.js

--- recycleBin/app/screen/ItemListScreen.js
+++ recycleBin/app/screen/ItemListScreen.js
@@ -1,41 +1,58 @@
-import { View, Text, StatusBar, StyleSheet, FlatList } from "react-native";
-import React from "react";
+import { View, Text, StatusBar, StyleSheet, FlatList,  } from "react-native";
+import React, { useEffect, useState } from "react";
 import { Card } from "react-native-elements";
 
 import { CardComponent } from "../components/CardComponent";
+import listingsAPI from "../api/listings";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
-const listings = [
-  {
-    id: 1,
-    title: "Red jacket for sale",
-    price: 100,
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg"),
-  },
-];
+import { AppText } from "../components/Text";
+import Button from "../components/Button";
+import ActivityIndicator from '../components/ActivityIndicator'
 
-export default function ItemListScreen({navigation}) {
+export default function ItemListScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const loadListings = async () => {
+    setLoading(true)
+    const response = await listingsAPI.getListings();
+    setLoading(false)
+    
+    if (!response.ok) return setError(true);
+    
+    setError(false);
+    setListings(response.data);
+  };
+
+  useEffect(() => {
+    loadListings();
+  }, []);
 
   return (
     <View style={styles.screen}>
       <StatusBar />
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings.</AppText>
+          <Button title="Retry" onPress={loadListings}></Button>
+        </>
+      )}
+      <ActivityIndicator visible={loading}  />
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
-        renderItem={({ item }) => 
+        renderItem={({ item }) => (
           <CardComponent
-          onPress={()=>{navigation.navigate(routes.LISTING_DETAILS, item )}}
-          title={item.title}
-          subTitle={"$" + item.price}
-          image={item.image}
-          /> 
-        }
+            onPress={() => {
+              navigation.navigate(routes.LISTING_DETAILS, item);
+            }}
+            title={item.title}
+            subTitle={"$" + item.sellingPrice}
+            imageUrl={item.images[0].url}
+          />
+        )}
       />
     </View>
   );
@@ -43,6 +60,7 @@ export default function ItemListScreen({navigation}) {
 
 const styles = StyleSheet.create({
   screen: {
+    flex:1,
     padding: 20,
     backgroundColor: colors.lightGrey,
   },
