220f3cbb399df23fad5dfdd35bc14613588ab8df
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2022-05-13 09:05:27 +0600

    .

--- recycleBin/app/screen/AccountScreen.js
+++ recycleBin/app/screen/AccountScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, FlatList,StatusBar } from "react-native";
+import { StyleSheet, View, FlatList, StatusBar } from "react-native";
 
 import { ListItem, ListItemSeparator } from "../components/lists";
 import colors from "../config/colors";
